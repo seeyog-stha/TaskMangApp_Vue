559be1e3c494049f409c22aa0ba08f7bba405245
seeyog-stha <jabeens.calcgen@gmail.com>
2025-02-16 15:19:47 +0545

    feat:initial commit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="container">
+
+    <header>
+      <div class="title">
+        <h2>Task Management System</h2>
+      </div>
+
+    </header>
+
+    <RouterView />
+  </div>
+</template>
+<style scoped>
+
+.container {
+  display: flex;
+  flex-flow: column;
+}
+
+header {
+  /* width: 100vw; */
+  background-color: #1484e4;
+  padding: 15px;
+  text-align: center;
+  position: sticky;
+  top:0;
+}
+
+header h2 {
+  font-size: 16px;
+  font-weight: bold;
+  color: white;
+  text-transform: uppercase;
+}
+
+.title{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <main>
+  <p>hi there</p>
+  </main>
+</template>
